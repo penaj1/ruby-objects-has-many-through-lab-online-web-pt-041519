63954ef480e63cf9016c188056517d8d5bb78e9f
Janice Pena <jipena94@gmail.com>
2019-05-26 07:27:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,9 +11,21 @@ class Patient
   def self.all
     ALL
   end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
 
-  def doctor
-    @doctor
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
   end
 
 end
