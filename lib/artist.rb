da27e9b27c4c91a89c212dd4057308321100aca7
Janice Pena <jipena94@gmail.com>
2019-05-26 07:08:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,23 @@
 class Artist
-  
-  ALL = []
- 
+
   attr_accessor :name, :artist, :genre
- 
+
+  ALL=[]
+
   def initialize(name)
     @name = name
     ALL << self
   end
- 
+
   def self.all
     ALL
   end
-  
+
   def new_song(name, genre)
-    songs = Song.new(name, self, genre)
+    song = Song.new(name, self, genre)
     self.songs.last
   end
-  
+
   def songs
     Song.all.select do |song|
       song.artist == self
@@ -29,5 +29,5 @@ class Artist
       song.genre
     end
   end
-  
+
 end
