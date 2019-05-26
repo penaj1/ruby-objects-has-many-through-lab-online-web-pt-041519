5ae0eadd7e4eb2e0c7edba887c252ad13abb8a9e
Janice Pena <jipena94@gmail.com>
2019-05-26 07:19:16 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,31 @@
 class Appointment
+
+  attr_reader :name, :genre
+  attr_accessor :song
+
+  ALL = []
   
+  def self.all
+    ALL
+  end
   
+  def initialize(name)
+    @name = name
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+  def patient
+    Patient.all.select do |patient|
+      patient.doctor == self
+    end
+  end
+
+  def doctor
+    Patient.all.map do |patient|
+      patient.doctor
+    end
+  end
 end
